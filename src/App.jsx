ea7ab6d1f1393f839556b0989b8d996abd4c5684
rafika khatun <rafikakhatun607@gmail.com>
2025-10-01 23:30:46 +0530

    create  CurrentWeather component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import CurrentWeather from './component/ CurrentWeather'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <CurrentWeather></CurrentWeather>
       
     </>
   )
